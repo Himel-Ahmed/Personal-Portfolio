3cdc6c9bcb000a3c96be3b0fc80d4abc777aad3f
Hemal Ahmed <146666169+Himel-Ahmed@users.noreply.github.com>
2023-10-04 12:43:43 +0600

    Add files via upload

---
+++ Personal Portfolio/playground.js
@@ -0,0 +1,9 @@
+function textAnimaton(){
+    var typed = new Typed(".autoType",{
+        strings : ["I'm Himel Ahmed","A Web Developer","Working With Front-End","Passionate about Coding","Created 10+ Awesome Project"],
+        typeSpeed:100,
+        backSpeed:25,
+        loop:true
+        });
+}
+textAnimaton();
